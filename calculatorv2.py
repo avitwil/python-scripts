964476f19aac145da98f064f1614a524da8f4859
avitwil <avitwil@gmail.com>
2025-06-07 03:05:48 +0300

    Update calculatorv2.py

--- calculatorv2.py
+++ calculatorv2.py
@@ -8,21 +8,29 @@ def calculator():
                math = j
     if i == 1:
         y = x.rpartition(math)
-        num1 = int(y[0])
-        num2 = int(y[2])
-        match math:
-            case "+":
-                num3 = num1 + num2
-            case "-":
-                num3 = num1 - num2
-            case "*":
-                num3 = num1 * num2
-            case "/":
-                num3 = num1 / num2
-            case _:
-                calculator()
-        print(str(num1) + " " + str(math) + " " + str(num2) + " = " + str(num3))
+        num1 = y[0]
+        num2 = y[2]
+        if  num1.isdigit() is False or num2.isdigit() is False:
+            print("please enter a valid calculation")
+            calculator()
+        else:
+            num1 = int(num1)
+            num2 = int(num2)
+            match math:
+                case "+":
+                    num3 = num1 + num2
+                case "-":
+                    num3 = num1 - num2
+                case "*":
+                    num3 = num1 * num2
+                case "/":
+                    num3 = num1 / num2
+                case _:
+                    print("please enter a valid calculation")
+                    calculator()
+            print(str(num1) + " " + str(math) + " " + str(num2) + " = " + str(num3))
     else:
+        print("please enter a valid calculation")
         calculator()
 
 def clear_screen():
@@ -41,7 +49,7 @@ while input('type "y" if you want to try again: ') =="y":
     clear_screen()
     print("======= welcome to avi's calculator =======")
     calculator()
-print("======= Thank you for using  avi's calculator =======")
+print("======= Thank you for using avi's calculator =======")
 
 
 
